e82d81e45b8f9d3e57c2527ef08a02708a7424c1
SRIRAM-POTHUMANI <84328532+SRIRAM-POTHUMANI@users.noreply.github.com>
2021-12-17 20:26:05 +0530

    commit

--- src/ChatPage.js
+++ src/ChatPage.js
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
+import Modal from "react-bootstrap/Modal";
 import "./ChatPage.css";
 import SpeakerNotesIcon from "@material-ui/icons/SpeakerNotes";
 import { InsertEmoticon, MicNone } from "@material-ui/icons";
 // import AccountCircleIcon from "@material-ui/icons/AccountCircle";
 
 function ChatPage() {
-  const [input, setinput] = useState([]);
-  const sendMessage = async (e) => {
-    e.preventDefault();
+  // const [input, setinput] = useState([]);
+  // const sendMessage = async (e) => {
+  //   e.preventDefault();
     // await axios.post("/messages/new", {
     // message: input,
     // name: name,
@@ -17,8 +18,9 @@ function ChatPage() {
     // });
     // setinput("");
     // scrollToBottom();
-  };
+  // };
 
+    const [modalShow, setModalShow] = React.useState(false);
   return (
     <div class="card" style={{ height: "100vh", overflow: "hidden" }}>
       {/* header */}
@@ -28,14 +30,25 @@ function ChatPage() {
       >
         <div class="d-flex flex-row bd-highlight mb-3">
           <div class="p-0 bd-highlight">
-            <SpeakerNotesIcon />
+            {/* <SpeakerNotesIcon> */}
+            <SpeakerNotesIcon onClick={() => setModalShow(true)}>
+              Launch vertically centered modal
+            </SpeakerNotesIcon>
+
+            <MyVerticallyCenteredModal
+              show={modalShow}
+              onHide={() => setModalShow(false)}
+            />
+            {/* </SpeakerNotesIcon> */}
           </div>
-          <div class="p-0 bd-highlight mx-auto fw-bold">Chat Name</div>
+          <div class="p-0 bd-highlight mx-auto fw-bold user-select-none">
+            Chat Name
+          </div>
         </div>
       </div>
       {/* body */}
       <div
-        class="card-body overflow-auto"
+        class="card-body overflow-auto user-select-none"
         style={{
           height: "90vh",
           marginTop: "2rem",
@@ -218,5 +231,36 @@ function ChatPage() {
     </div>
   );
 }
+
+
+
+function MyVerticallyCenteredModal(props) {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Modal heading
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+}
+
 
 export default ChatPage;
