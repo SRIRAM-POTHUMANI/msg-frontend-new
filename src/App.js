c774a53d7de506c0d1bf3c249f3e48c8c81cd6cd
SRIRAM-POTHUMANI <84328532+SRIRAM-POTHUMANI@users.noreply.github.com>
2021-12-18 01:44:07 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-// import "./App.css";
+import "./App.css";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ChatPage from "./ChatPage";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import ChatPage from "./ChatPage";
 import Login from "./Login";
-import RegisterUser from "./RegisterUser";
+// import RegisterUser from "./RegisterUser";
 function App() {
   return (
     <div className="App">
-      <Router>
+      {/* <Router>
         <Routes>
-          <Route path="/" element={<ChatPage />} />
+          <Route path="/" element={<Login />} />
         </Routes>
         <Routes>
           <Route path="/reg" element={<RegisterUser />} />
         </Routes>
         <Routes>
-          <Route path="/chat" element={<Login />} />
+          <Route path="/chat" element={<ChatPage />} />
         </Routes>
-      </Router>
+      </Router> */}
+      <Login />
     </div>
   );
 }
